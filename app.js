eea9834f3ae3d430e5bf4f652641374539db63ce
EmaDiaz01 <demanuel269@gmail.com>
2022-06-26 12:06:24 -0300

    adjunto tp para revision

---
+++ app.js
@@ -0,0 +1,16 @@
+const express=require('express')
+const path = require('path')
+const app= express()
+const port = 3031
+
+app.listen(port,()=>console.log('Server running in port http://localhost:'+ port))
+
+
+app.use(express.static('public'))
+
+app.get("/", (req, res)=>res.sendFile(path.join(__dirname,'views', 'homes.html' )))
+
+
+
+
+
